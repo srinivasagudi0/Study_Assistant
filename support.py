3397b0eddf3060381acd731319115d0208b5d6f2
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-17 12:09:03 -0500

    Add pdf_to_text function to convert PDF to text
    should hopefully work

--- support.py
+++ support.py
@@ -1,1 +1,12 @@
 # all important functions and intel stays here
+
+# first task convert pdf to text
+import PyPDF2
+
+def pdf_to_text(pdf_file):
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
