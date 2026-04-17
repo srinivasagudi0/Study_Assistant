7dbc975b4c8cae8ae902d3fcf8e2ed794ebe53a5
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-17 12:14:10 -0500

    Uncomment test lines because the feature is just in beta

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ st.header("Welcome to Smart Study Copilot!")
 # be able to take in user input as a pdf first and print out the text content of the pdf
 st.subheader("Upload your PDF")
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-#if uploaded_file is not None:
-#    text = pdf_to_text(uploaded_file)
-#    with st.spinner("Extracting text from PDF..."):
-#        st.write(text)
-
 if uploaded_file is not None:
-    st.write("File uploaded successfully!")
+    text = pdf_to_text(uploaded_file)
+    with st.spinner("Extracting text from PDF..."):
+        st.write(text)
+# should be good
